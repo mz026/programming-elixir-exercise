4ccd64cb36a06c4d81ae53f5f25d523783548026
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2016-01-02 15:43:33 +0800

    refactor guess

--- ch6/guess.exs
+++ ch6/guess.exs
@@ -3,17 +3,19 @@ defmodule Chop do
     guess_val = div(low + high, 2)
 
     IO.puts "is it #{guess_val}?"
-
-    if guess_val == answer do
-      IO.puts answer
-    else
-      if guess_val > answer do
-        guess(answer, low..guess_val)
-      else
-        guess(answer, guess_val..high)
-      end
-    end
+    test(answer, low..high, guess_val)
   end
 
+  def test(answer, _, guess_val) when guess_val == answer do
+    IO.puts answer
+  end
+
+  def test(answer, low.._, guess_val) when guess_val > answer do
+    guess(answer, low..guess_val)
+  end
+
+  def test(answer, _..high, guess_val) when guess_val < answer do
+    guess(answer, guess_val..high)
+  end
 
 end
