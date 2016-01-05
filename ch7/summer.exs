936013d7526ed0fcca08e41066d8e72e82edca88
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2016-01-05 09:24:16 +0800

    add map, mapsum

--- ch7/summer.exs
+++ ch7/summer.exs
@@ -5,4 +5,12 @@ defmodule Summer do
   def sum_w_acc(list, total \\ 0)
   def sum_w_acc([], total), do: total
   def sum_w_acc([head | tail], total), do: sum_w_acc(tail, head + total)
+
+  def map([], _func), do: []
+  def map([head | tail], func), do: [func.(head) | map(tail, func)]
+
+  def mapsum list, func do
+    l = map(list, func)
+    sum(l)
+  end
 end
