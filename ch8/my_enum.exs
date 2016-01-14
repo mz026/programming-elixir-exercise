9c2753fcbee9a6ef25d2c34627e307729c8d36ea
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2016-01-14 22:58:21 +0800

    add filter

--- ch8/my_enum.exs
+++ ch8/my_enum.exs
@@ -11,4 +11,17 @@ defmodule MyEnum do
     list
   end
 
+  def filter(list, func) do
+    Enum.reverse(_filter(list, func, []))
+  end
+
+  def _filter([], _func, selected), do: selected
+  def _filter([head|tail], func, selected) do
+    if func.(head) do
+      _filter(tail, func, [head|selected])
+    else
+      _filter(tail, func, selected)
+    end
+  end
+
 end
