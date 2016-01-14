027bd37cf10afb5e8753d0df76427d128685c36c
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2016-01-14 23:08:29 +0800

    add split

--- ch8/my_enum.exs
+++ ch8/my_enum.exs
@@ -15,13 +15,24 @@ defmodule MyEnum do
     Enum.reverse(_filter(list, func, []))
   end
 
-  def _filter([], _func, selected), do: selected
-  def _filter([head|tail], func, selected) do
+  defp _filter([], _func, selected), do: selected
+  defp _filter([head|tail], func, selected) do
     if func.(head) do
       _filter(tail, func, [head|selected])
     else
       _filter(tail, func, selected)
     end
+  end
+
+  def split(list, number) do
+    _split(list, number, [])
+  end
+
+  defp _split([head|tail], number, current_left) when length(current_left) < number do
+    _split(tail, number, [head|current_left])
+  end
+  defp _split(list, number, current_left) when length(current_left) >= number do
+    { Enum.reverse(current_left), list }
   end
 
 end
