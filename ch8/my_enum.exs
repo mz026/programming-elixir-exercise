82481da6dbe09ebd1f63bfa17279fbe94d6a92e9
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2016-01-14 23:45:23 +0800

    refactor codes

--- ch8/my_enum.exs
+++ ch8/my_enum.exs
@@ -47,17 +47,12 @@ defmodule MyEnum do
     current
   end
 
-  def flatten([]) do
-    IO.puts "empty case"
-    []
-  end
+  def flatten([]), do: []
+
   def flatten([head|tail]) do
-    IO.puts "normal case. head: #{head}, tail: #{tail}"
     Enum.concat(flatten(head), flatten(tail))
   end
-  def flatten(el) do
-    IO.puts "element case. el: #{el}"
-    [ el ]
-  end
+
+  def flatten(el), do: [ el ]
 
 end
